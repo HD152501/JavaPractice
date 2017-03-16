bcdb663354e35fef1c0339b3eabe42e6c622e1e4
sun90 <sun90@DESKTOP-SUQSC42>
2017-03-16 10:00:41 +0900

    실습과제 1. 아이유 프로필 출력하기

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * JavaPractice
+ *   └ PrimitiveDataType
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 16.
+ * </pre>
+ *
+ * @author : 강선구
+ * @version : 1.0
+ */
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		
+	}
+}
