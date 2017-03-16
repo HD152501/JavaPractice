a8b52c408cc5a16c9208480d02e239d9c6a066f2
sun90 <sun90@DESKTOP-SUQSC42>
2017-03-16 16:40:03 +0900

    실습과제 1. 아이유 프로필 출력하기

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -18,6 +18,26 @@ package org.dimigo.basic;
 public class PrimitiveDataType {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("<< 아이유 프로필 >>");
+
+		String name = "아이유";
+		System.out.println("이름 : " + name);
+
+		boolean isMale = false;
+		System.out.print("성별 : ");
+		System.out.println(isMale ? "남자" : "여자");
+
+		int age = 25;
+		System.out.println("나이 : " + age + "세");
+
+		double height = 161.8;
+		System.out.println("키 : " + height + "cm");
+
+		float weight = 44.3F;
+		System.out.println("키 : " + weight + "kg");
+
+		char bloodType = 'A';
+		System.out.println("혈액형 : " + bloodType + "형");
 	}
 }
