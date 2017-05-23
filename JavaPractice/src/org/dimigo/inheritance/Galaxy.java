faf1cd747ffe0c05792ed19f33b2e396c0ece2bd
HD152501 <kang78621>
2017-05-23 12:09:24 +0900

    실습과제 11. 스마트폰 경쟁

--- JavaPractice/src/org/dimigo/inheritance/Galaxy.java
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -1,35 +1,26 @@
 /**
- * 
+ *
  */
-package prac11;
+package org.dimigo.inheritance;
 
 /**
-* <pre>
-* prac11
-*	|_ Galaxy
-* 
-* 1. 개요 : 
-* 2. 작성일 : 2017. 5. 16.
-*<pre>
-*
-*@author         : USER
-*@version        : 1.0
-*/
+ * Created by sun90 on 2017-05-16.
+ */
 public class Galaxy extends SmartPhone {
 
-	public Galaxy() {
-		
-	}
-	
-	public Galaxy(String model, String company, int price) {
-		super(model, company, price);
-	}
-	
-	public void pay() {
-		System.out.println("삼성 페이로 결제합니다.");
-	}
-	
-	public void useWirelessCharging() {
-		System.out.println("무선 충전 기능을 사용합니다.");
-	}
+    public Galaxy() {
+
+    }
+
+    public Galaxy(String model, String company, int price) {
+        super(model, company, price);
+    }
+
+    public void pay() {
+        System.out.println("삼성 페이로 결제합니다.");
+    }
+
+    public void useWirelessCharging() {
+        System.out.println("무선 충전 기능을 사용합니다.");
+    }
 }
--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -1,36 +1,27 @@
 /**
- * 
+ *
  */
-package prac11;
+package org.dimigo.inheritance;
 
 /**
-* <pre>
-* prac11
-*	|_ SmartPhoneTest
-* 
-* 1. 개요 : 
-* 2. 작성일 : 2017. 5. 16.
-*<pre>
-*
-*@author         : USER
-*@version        : 1.0
-*/
+ * Created by sun90 on 2017-05-16.
+ */
 public class SmartPhoneTest {
-	
-	public static void main(String[] args) {
-		
-		SmartPhone[] sp = {
-			new IPhone("iPhone 7", "애플", 900000),
-			new Galaxy("갤럭시 S8", "삼성", 800000)
-		};
-		
-		for(SmartPhone sps : sp) {
-			System.out.println(sps);
-			sps.turnOn();
-			sps.pay();
-			sps.useSpecialFunction(sps);
-			sps.turnOff();
-			System.out.println();
-		}
-	}
+
+    public static void main(String[] args) {
+
+        SmartPhone[] sp = {
+                new IPhone("iPhone 7", "애플", 900000),
+                new Galaxy("갤럭시 S8", "삼성", 800000)
+        };
+
+        for (SmartPhone sps : sp) {
+            System.out.println(sps);
+            sps.turnOn();
+            sps.pay();
+            sps.useSpecialFunction(sps);
+            sps.turnOff();
+            System.out.println();
+        }
+    }
 }
